762ba473295bd73d4285bdadc7fc17a724e4c612
ririari <ririari@gmail.com>
2012-10-01 03:15:52 -0300

    ctrl+shift+f…

--- RPCStandaloneServer/src/SimpleXMLRpcTest.java
+++ RPCStandaloneServer/src/SimpleXMLRpcTest.java
@@ -1,21 +1,12 @@
-public class SimpleXMLRpcTest
-{
-	public String hello()
-	{
+public class SimpleXMLRpcTest {
+	public String hello() {
 		return "hello world";
 	}
- 
-	public static void main(String[] args) throws Exception
-	{
+
+	public static void main(String[] args) throws Exception {
 		System.out.println("starting server");
 		SimpleXmlRpcServer server = new SimpleXmlRpcServer(7777);
 		server.addHandler("zen", new SimpleXMLRpcTest());
 		server.serve_forever();
- 
-		while (true)
-		{
-			System.out.println("");
-			Thread.sleep(60 * 60 * 60);
-		}
 	}
 }
--- RPCStandaloneServer/src/SimpleXmlRpcRequestHandlerFactory.java
+++ RPCStandaloneServer/src/SimpleXmlRpcRequestHandlerFactory.java
@@ -7,23 +7,33 @@ import org.apache.xmlrpc.server.RequestProcessorFactoryFactory;
 import org.apache.xmlrpc.server.RequestProcessorFactoryFactory.RequestProcessorFactory;
 
 /**
- * Simple XMLRPC request handler factory - this little magic class allows
- * for a request INSTANCE to be set as the handler for the xml rpc server
- * Ideally, a client shouldn't even NEED to know what this is doing.
+ * Simple XMLRPC request handler factory - this little magic class allows for a
+ * request INSTANCE to be set as the handler for the xml rpc server Ideally, a
+ * client shouldn't even NEED to know what this is doing.
  */
-public class SimpleXmlRpcRequestHandlerFactory
-implements RequestProcessorFactoryFactory,RequestProcessorFactory
-{
+public class SimpleXmlRpcRequestHandlerFactory implements
+		RequestProcessorFactoryFactory, RequestProcessorFactory {
 	private Map handlerMap = new HashMap();
-	public void setHandler(String name, Object handler) { this.handlerMap.put(name, handler); }
-	public Object getHandler(String name) { return this.handlerMap.get(name); }
- 
+
+	public void setHandler(String name, Object handler) {
+		this.handlerMap.put(name, handler);
+	}
+
+	public Object getHandler(String name) {
+		return this.handlerMap.get(name);
+	}
+
 	public RequestProcessorFactory getRequestProcessorFactory(Class arg0)
-	throws XmlRpcException { return this; }
-	public Object getRequestProcessor(XmlRpcRequest request) throws XmlRpcException
-	{
-		String handlerName = request.getMethodName().substring(0,request.getMethodName().lastIndexOf("."));
-		if( !handlerMap.containsKey(handlerName)) throw new XmlRpcException("Unknown handler: "+handlerName);
+			throws XmlRpcException {
+		return this;
+	}
+
+	public Object getRequestProcessor(XmlRpcRequest request)
+			throws XmlRpcException {
+		String handlerName = request.getMethodName().substring(0,
+				request.getMethodName().lastIndexOf("."));
+		if (!handlerMap.containsKey(handlerName))
+			throw new XmlRpcException("Unknown handler: " + handlerName);
 		return handlerMap.get(handlerName);
 	}
 }
